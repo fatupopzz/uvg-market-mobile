e8e362061880b6349d276bd7f13b59599a138929
josesan28 <san24092@uvg.edu.gt>
2025-09-10 23:51:16 -0600

    Se cambió el ícono del botón de chat en el inicio

--- app/src/main/java/com/example/uvgmarket/pantallainicio/marketplace/MarketplaceScreen.kt
+++ app/src/main/java/com/example/uvgmarket/pantallainicio/marketplace/MarketplaceScreen.kt
@@ -9,11 +9,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -24,8 +23,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.uvgmarket.R
 import com.example.uvgmarket.pantallainicio.components.CustomSearchBar
 import com.example.uvgmarket.pantallainicio.components.ProfileAvatar
 import com.example.uvgmarket.pantallainicio.components.Entrepreneur
@@ -63,8 +64,9 @@ fun MarketplaceScreen(
                 contentColor = AppColors.TextWhite
             ) {
                 Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "Agregar emprendedor"
+                    modifier = Modifier.size(32.dp),
+                    painter = painterResource(id = R.drawable.chat),
+                    contentDescription = "Ver chat"
                 )
             }
         }
